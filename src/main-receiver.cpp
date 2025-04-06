6ddd0a99443fcbd9c429a52da9b8168f5b6e1fc0
jlucier <jordan@jordanlucier.com>
2025-04-06 19:25:52 -0400

    Mostly working with button, but can't seem to control the builtin LED at the same time.

--- src/main-receiver.cpp
+++ src/main-receiver.cpp
@@ -3,26 +3,48 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
-// #define TX 22
-// #define RX 21
-
 SerialMP3Player mp3(RX, TX);
 
+struct message_t {
+    char data[32];
+};
+
+message_t msg;
+
+void play_sound() {
+    digitalWrite(LED_BUILTIN, HIGH);
+    mp3.play(1, 10);
+    digitalWrite(LED_BUILTIN, LOW);
+}
+
+
+void on_recv(uint8_t* mac, uint8_t* data, uint8_t len) {
+    memcpy(&msg, data, sizeof(msg));
+    Serial.println(msg.data);
+    play_sound();
+}
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
-  mp3.begin(9600);
-  delay(500);
-  mp3.sendCommand(CMD_SEL_DEV, 0, 2);   //select sd-card
-  delay(500);
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    digitalWrite(LED_BUILTIN, LOW);
+
+    Serial.begin(9600);
+    mp3.begin(9600);
+    delay(500);
+    mp3.sendCommand(CMD_SEL_DEV, 0, 2);   //select sd-card
+    delay(500);
+
+    WiFi.mode(WIFI_STA);
+
+    if (esp_now_init() != OK) {
+        Serial.println("ESP-NOW init failed!");
+        return;
+    }
+
+    esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+    esp_now_register_recv_cb(on_recv);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  mp3.play(1, 10);
-  delay(3000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
 }
--- src/main-sender.cpp
+++ src/main-sender.cpp
@@ -2,16 +2,53 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
+#define BUTTON_PIN D3
+#define OFF HIGH
+#define ON LOW
+
+uint8_t RECEVIER_MAC[] = {0x58, 0xbf, 0x25, 0xdb, 0x10, 0x11};
+char msg[] = "Hello World!";
+int button_state = OFF;
+
+void on_sent(uint8_t *mac_addr, uint8_t sendStatus) {
+    Serial.print("\r\nDelivery Status: ");
+    Serial.println(sendStatus == 0 ? "Delivered Successfully" : "Delivery Fail");
+}
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT);
+
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+
+    Serial.begin(9600);
+    WiFi.mode(WIFI_STA);
+
+    if (esp_now_init() != OK) {
+        Serial.println("ESP-NOW init failed!");
+        return;
+    }
+    esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+
+    esp_now_register_send_cb(on_sent);
+
+    esp_now_add_peer(RECEVIER_MAC, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+    int new_state = digitalRead(BUTTON_PIN);
+
+    if (new_state != button_state) {
+        Serial.print("Changed: ");
+        Serial.println(new_state == ON ? "ON" : "OFF");
+        digitalWrite(LED_BUILTIN, ON ? HIGH : LOW);
+
+        if (new_state == ON) {
+            esp_now_send(RECEVIER_MAC, (uint8_t*)msg, sizeof(msg));
+        }
+    }
+
+    delay(50);
+    button_state = new_state;
 }
